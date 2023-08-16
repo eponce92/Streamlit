e2bf195f090c197d1f18794bc8c9ab4a2a9a8f32
bfufu <64049844+eponce92@users.noreply.github.com>
2023-08-16 08:08:24 -0700

    Update simulator.py

--- simulator.py
+++ simulator.py
@@ -1,22 +1,22 @@
 import streamlit as st
 
-def create_graph(num_stations):
+def create_graph():
     dot_string = 'digraph {rankdir=LR;'
     dot_string += '"Line Input" -> C1;'
-    for i in range(1, num_stations + 1):
-        dot_string += f'S{i} [label="Station {i}"];'
-        dot_string += f'C{i} [shape=rectangle, label="Conveyance {i}"];'
-        dot_string += f'C{i} -> S{i};'
-        if i < num_stations:
-            dot_string += f'S{i} -> C{i+1};'
-        else:
-            dot_string += f'S{i} -> "Line Output";'
+    dot_string += 'C1 -> S1_R1; C1 -> S1_R2;'
+    dot_string += 'S1_R1 -> C2; S1_R2 -> C2;'
+    dot_string += 'S1_R1 [label="Station 1  60s $40,000.00"];'
+    dot_string += 'S1_R2 [label="Station 1  60s $40,000.00"];'
+    dot_string += 'C2 -> S2_R1;'
+    dot_string += 'S2_R1 [label="Station 2  60s $40,000.00"];'
+    dot_string += 'S2_R1 -> "Line Output";'
     dot_string += '"Line Output" [shape=ellipse];'
+    dot_string += 'C1 [shape=rectangle, label="Conveyance 1"];'
+    dot_string += 'C2 [shape=rectangle, label="Conveyance 2"];'
     dot_string += '}'
     return dot_string
 
 st.title("Manufacturing Line Simulation")
-num_stations = st.sidebar.number_input("Number of Stations", value=2, min_value=1)
 
-graph_dot_string = create_graph(num_stations)
+graph_dot_string = create_graph()
 st.graphviz_chart(graph_dot_string)
