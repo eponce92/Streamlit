35ae6710fc3f51752d9c2bcfa5eb4050d4d16e7a
bfufu <64049844+eponce92@users.noreply.github.com>
2023-08-15 19:06:15 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,18 +1,54 @@
 import streamlit as st
 
-# Input fields for the parameters
-shift_length_hours = st.number_input("Shift Length (hours):", min_value=1)
-shifts_per_day = st.number_input("Number of Shifts per Day:", min_value=1)
-cycle_time_per_machine_seconds = st.number_input("Cycle Time per Machine (seconds):", min_value=1)
-number_of_stations = st.number_input("Number of Stations:", min_value=1)
-total_budget = st.number_input("Total Budget ($):", min_value=1)
+# Function to perform calculations
+def calculate_values(cycle_time_per_machine, number_of_stations, total_budget):
+    shift_length_hours = 12
+    shifts_per_day = 2
+    shift_length_seconds = shift_length_hours * 60 * 60
+    pills_per_shift_per_machine = shift_length_seconds / cycle_time_per_machine
+    pills_per_shift_entire_line = pills_per_shift_per_machine / number_of_stations
+    daily_output = pills_per_shift_entire_line * shifts_per_day
+    budget_per_station = total_budget / number_of_stations
+    cycle_time_per_station = cycle_time_per_machine
+    cycle_time_entire_process = cycle_time_per_station * number_of_stations
+    return daily_output, budget_per_station, cycle_time_per_station, cycle_time_entire_process
 
-# Perform calculations (similar to what we've done earlier)
-# ...
+# Layout
+st.title("Manufacturing Line Comparison Tool")
+st.write("Compare two different manufacturing line options by modifying the values below.")
+
+# Input for Option 1
+st.sidebar.subheader("Option 1")
+cycle_time_per_machine_1 = st.sidebar.number_input("Cycle Time per Machine (seconds):", min_value=1, value=60)
+number_of_stations_1 = st.sidebar.number_input("Number of Stations:", min_value=1, value=14)
+total_budget_1 = st.sidebar.number_input("Total Budget ($):", min_value=0, value=800000)
+
+# Input for Option 2
+st.sidebar.subheader("Option 2")
+cycle_time_per_machine_2 = st.sidebar.number_input("Cycle Time per Machine (seconds):", min_value=1, value=60)
+number_of_stations_2 = st.sidebar.number_input("Number of Stations:", min_value=1, value=14)
+total_budget_2 = st.sidebar.number_input("Total Budget ($):", min_value=0, value=800000)
+
+# Calculations for Option 1
+daily_output_1, budget_per_station_1, cycle_time_per_station_1, cycle_time_entire_process_1 = calculate_values(cycle_time_per_machine_1, number_of_stations_1, total_budget_1)
+
+# Calculations for Option 2
+daily_output_2, budget_per_station_2, cycle_time_per_station_2, cycle_time_entire_process_2 = calculate_values(cycle_time_per_machine_2, number_of_stations_2, total_budget_2)
 
 # Display results
-st.write(f"Daily Output: {daily_output} pills")
-st.write(f"Budget per Station: ${budget_per_station}")
-st.write(f"Cycle Time per Station: {cycle_time_per_station} seconds")
-st.write(f"Cycle Time for Entire Process: {cycle_time_entire_process} seconds")
+st.subheader("Option 1 vs Option 2")
+cols = st.beta_columns(2)
 
+with cols[0]:
+    st.markdown("**Option 1**")
+    st.write("Daily Output:", daily_output_1, "pills")
+    st.write("Budget per Station: $", budget_per_station_1)
+    st.write("Cycle Time per Station:", cycle_time_per_station_1, "seconds")
+    st.write("Cycle Time for Entire Process:", cycle_time_entire_process_1, "seconds")
+
+with cols[1]:
+    st.markdown("**Option 2**")
+    st.write("Daily Output:", daily_output_2, "pills")
+    st.write("Budget per Station: $", budget_per_station_2)
+    st.write("Cycle Time per Station:", cycle_time_per_station_2, "seconds")
+    st.write("Cycle Time for Entire Process:", cycle_time_entire_process_2, "seconds")
