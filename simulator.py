308965b4bda86821e77dd54971505e3a45fc6e99
bfufu <64049844+eponce92@users.noreply.github.com>
2023-08-15 21:52:46 -0700

    Update simulator.py

--- simulator.py
+++ simulator.py
@@ -1,40 +1,22 @@
-import streamlit as st
+import streamlit as sthttps://github.com/eponce92/Streamlit/blob/main/simulator.py
 
-def create_ascii_representation(stations, connections):
-    ascii_lines = []
-    current_line = ""
-    max_station_length = max(len(f"[Station {i+1}  {s['cycle_time']}s ${s['budget']/1000:.2f}k]") for i, s in enumerate(stations))
+def create_ascii_representation(stations, groups):
+    ascii_lines = ["[Line Input]"]
     
-    for connection in connections:
-        from_group = connection['from_group']
-        to_group = connection['to_group']
-        
-        from_stations = ", ".join([f"Station {i+1}" for i in from_group])
-        to_stations = ", ".join([f"Station {i+1}" for i in to_group])
-        
-        station_info = f"[{from_stations}]".ljust(max_station_length)
-        next_station_info = f"[{to_stations}]".ljust(max_station_length)
-        
-        current_line = station_info + " --+"
-        ascii_lines.append(current_line)
-        
-        for to_station in to_group:
-            ascii_lines.append(" " * (len(current_line) - 1) + "|")
-            station_info = f"[Station {to_station+1}  {stations[to_station]['cycle_time']}s ${stations[to_station]['budget']/1000:.2f}k]".ljust(max_station_length)
-            current_line = " " * (len(current_line) - 1) + "+-- " + station_info
-            ascii_lines.append(current_line)
+    for group in groups:
+        group_stations = " --+ ".join([f"[Station {i+1}  {stations[i]['cycle_time']}s ${stations[i]['budget']/1000:.2f}k]" for i in group])
+        ascii_lines.append(group_stations + " --+")
     
-    # Add final output line
-    ascii_lines.append(" " * (len(current_line) - 1) + "+--> [Final Output]")
+    ascii_lines.append("[Line Output]")
 
     return "\n".join(ascii_lines)
 
-def calculate_cycle_time_and_budget(stations, connections):
+def calculate_cycle_time_and_budget(stations, groups):
     total_cycle_time = 0
     total_budget = sum(station['budget'] for station in stations)
     
-    for connection in connections:
-        parallel_cycle_times = [stations[i]['cycle_time'] for i in connection['to_group']]
+    for group in groups:
+        parallel_cycle_times = [stations[i]['cycle_time'] for i in group]
         total_cycle_time += max(parallel_cycle_times)
     
     return total_cycle_time, total_budget
@@ -43,7 +25,7 @@ def calculate_cycle_time_and_budget(stations, connections):
 st.title("Manufacturing Line Simulator")
 
 # Define stations
-num_stations = st.number_input("Number of Stations:", min_value=1, value=4, format="%d")
+num_stations = st.number_input("Number of Stations:", min_value=1, value=3, format="%d")
 stations = []
 for i in range(num_stations):
     st.subheader(f"Station {i+1}")
@@ -51,27 +33,22 @@ for i in range(num_stations):
     budget = st.number_input(f"Budget for Station {i+1} ($):", min_value=0, value=100000, format="%d")
     stations.append({'cycle_time': cycle_time, 'budget': budget})
 
-# Define connections
-st.subheader("Connections")
-connections = []
-from_group = st.multiselect("Select stations for Group 1 (e.g., Station 1):", options=[f"Station {i+1}" for i in range(num_stations)])
-to_group = st.multiselect("Select stations for Group 2 (parallel with Group 1):", options=[f"Station {i+1}" for i in range(num_stations) if f"Station {i+1}" not in from_group])
-next_station = st.selectbox("Select next station connected in series:", options=[f"Station {i+1}" for i in range(num_stations) if f"Station {i+1}" not in from_group and f"Station {i+1}" not in to_group])
-
-from_group_indices = [int(station.split()[-1]) - 1 for station in from_group]
-to_group_indices = [int(station.split()[-1]) - 1 for station in to_group]
-next_station_index = int(next_station.split()[-1]) - 1
-
-connections.append({'from_group': from_group_indices, 'to_group': to_group_indices})
-connections.append({'from_group': to_group_indices, 'to_group': [next_station_index]})
+# Define groups
+num_groups = st.number_input("Number of Groups:", min_value=1, value=2, format="%d")
+groups = []
+for i in range(num_groups):
+    st.subheader(f"Group {i+1}")
+    group_stations = st.multiselect(f"Select stations for Group {i+1}:", options=[f"Station {j+1}" for j in range(num_stations)])
+    group_indices = [int(station.split()[-1]) - 1 for station in group_stations]
+    groups.append(group_indices)
 
 # Calculate and display results
-total_cycle_time, total_budget = calculate_cycle_time_and_budget(stations, connections)
+total_cycle_time, total_budget = calculate_cycle_time_and_budget(stations, groups)
 st.subheader("Results")
 st.write("Total Cycle Time:", total_cycle_time, "seconds")
 st.write("Total Budget: $", "{:,.2f}".format(total_budget))
 
 # Display ASCII representation
 st.subheader("Manufacturing Line Representation")
-ascii_representation = create_ascii_representation(stations, connections)
+ascii_representation = create_ascii_representation(stations, groups)
 st.text(ascii_representation)
