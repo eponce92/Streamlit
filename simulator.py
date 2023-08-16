8ec1fa71818bf9d9d8180ee8dc7ae49d8c3d4bd5
bfufu <64049844+eponce92@users.noreply.github.com>
2023-08-16 08:10:05 -0700

    Update simulator.py

--- simulator.py
+++ simulator.py
@@ -1,22 +1,40 @@
 import streamlit as st
 
-def create_graph():
+# Constants
+SHIFT_LENGTH_HOURS = 12
+NUM_SHIFTS_PER_DAY = 2
+SECONDS_PER_HOUR = 3600
+
+# User Inputs Section
+st.title("Manufacturing Line Simulation")
+st.sidebar.header("User Inputs")
+num_stations = st.sidebar.number_input("Number of Stations", value=2, min_value=1)
+cycle_times = [
+    st.sidebar.number_input(f"Cycle Time for Station {i+1} (seconds)", value=60, min_value=1) for i in range(num_stations)
+]
+budgets = [
+    st.sidebar.number_input(f"Budget for Station {i+1} ($)", value=40000.00, min_value=0.00, format="%.2f") for i in range(num_stations)
+]
+redundancies = [
+    st.sidebar.number_input(f"Redundancy Level for Station {i+1}", value=1, min_value=1) for i in range(num_stations)
+]
+
+# Graph Creation Function
+def create_graph(cycle_times, budgets, redundancies):
     dot_string = 'digraph {rankdir=LR;'
     dot_string += '"Line Input" -> C1;'
-    dot_string += 'C1 -> S1_R1; C1 -> S1_R2;'
-    dot_string += 'S1_R1 -> C2; S1_R2 -> C2;'
-    dot_string += 'S1_R1 [label="Station 1  60s $40,000.00"];'
-    dot_string += 'S1_R2 [label="Station 1  60s $40,000.00"];'
-    dot_string += 'C2 -> S2_R1;'
-    dot_string += 'S2_R1 [label="Station 2  60s $40,000.00"];'
-    dot_string += 'S2_R1 -> "Line Output";'
+    for i in range(num_stations):
+        for r in range(redundancies[i]):
+            dot_string += f'C{i+1} -> S{i+1}_R{r+1};'
+            label = f'"Station {i+1}  {cycle_times[i]}s ${budgets[i]:,.2f}"'
+            dot_string += f'S{i+1}_R{r+1} [label={label}];'
+            dot_string += f'S{i+1}_R{r+1} -> C{i+2};'
+        dot_string += f'C{i+1} [shape=rectangle, label="Conveyance {i+1}"];'
     dot_string += '"Line Output" [shape=ellipse];'
-    dot_string += 'C1 [shape=rectangle, label="Conveyance 1"];'
-    dot_string += 'C2 [shape=rectangle, label="Conveyance 2"];'
+    dot_string += f'C{num_stations+1} -> "Line Output";'
     dot_string += '}'
     return dot_string
 
-st.title("Manufacturing Line Simulation")
-
-graph_dot_string = create_graph()
+# Display Graphical Representation
+graph_dot_string = create_graph(cycle_times, budgets, redundancies)
 st.graphviz_chart(graph_dot_string)
