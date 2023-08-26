2c43f0c024cc722082ba3b5f39c8dc0b15f62873
bfufu <64049844+eponce92@users.noreply.github.com>
2023-08-26 15:31:12 -0700

    Update gecko_tape_gcode.py

--- gecko_tape_gcode.py
+++ gecko_tape_gcode.py
@@ -6,7 +6,6 @@ import base64
 def generate_gcode(angle, spacing, depth, pattern_length, feed_rate):
     num_grooves = int(pattern_length / spacing)
     x_move = depth * np.tan(np.radians(angle))
-
     gcode = ["G90 ; Set to absolute positioning",
              "G92 X0 Z0 ; Set current position as zero"]
     current_x = 0
@@ -30,22 +29,18 @@ def plot_gcode(x_values, z_values, title, x_range=None, y_range=None):
 st.title("G-code Generator for Groove Cutting")
 
 # Organize layout with columns
-col1, col2, col3 = st.columns(3)
-
+col1, col2, col3 = st.columns([2,3,3])  # Adjusting column widths
 
 with col1:
     angle = st.slider("Angle of Approach (degrees from vertical, 0-90)", 0, 90, 45)
     spacing = st.number_input("Distance Between Grooves (mm)", 0.000001, 10.0, 0.061, format="%.6f")
     depth = st.number_input("Depth of Grooves (mm)", 0.000001, 10.0, 0.102, format="%.6f")
-
-with col2:
     pattern_length = st.number_input("Length of Pattern (mm)", 0.000001, 500.0, 40.0, format="%.6f")
     feed_rate = st.number_input("Feed Rate (mm/min)", 1, 5000, 100)
-
-with col3:
+    
     if st.button("Generate G-code"):
         gcode_output = generate_gcode(angle, spacing, depth, pattern_length, feed_rate)
-        st.text_area("Generated G-code:", "\n".join(gcode_output), height=250)
+        st.text_area("Generated G-code:", "\n".join(gcode_output), height=500)
 
         x_values = [0]
         z_values = [0]
@@ -62,11 +57,13 @@ with col3:
                 x_values.append(current_x)
                 z_values.append(current_z)
 
-        st.plotly_chart(plot_gcode(x_values, z_values, "Side View of the G-code Path"))
+        with col2:
+            st.plotly_chart(plot_gcode(x_values, z_values, "Side View of the G-code Path"), use_container_width=True)
 
-        zoomed_x = x_values[:10]
-        zoomed_z = z_values[:10]
-        st.plotly_chart(plot_gcode(zoomed_x, zoomed_z, "Zoomed-in View of the First Few Grooves"))
+        with col3:
+            zoomed_x = x_values[:10]
+            zoomed_z = z_values[:10]
+            st.plotly_chart(plot_gcode(zoomed_x, zoomed_z, "Zoomed-in View of the First Few Grooves"), use_container_width=True)
 
         b64 = base64.b64encode("\n".join(gcode_output).encode()).decode()
         st.markdown(f"<a href=\"data:file/txt;base64,{b64}\" download=\"grooves.gcode\">Download G-code</a>", unsafe_allow_html=True)
