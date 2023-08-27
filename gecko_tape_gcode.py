5be25b6342e960d011a943a2654a58d5d94fb89c
bfufu <64049844+eponce92@users.noreply.github.com>
2023-08-26 18:25:31 -0700

    Update gecko_tape_gcode.py

--- gecko_tape_gcode.py
+++ gecko_tape_gcode.py
@@ -64,16 +64,17 @@ def generate_gcode(angle, spacing, depth, pattern_length, feed_rate, move_rate, 
     current_x = 0
 
     # Initial positioning to the start of the first groove
-    gcode.append(f"G1 X{x_move:.6f} Z0 F{move_rate}")
+    gcode.append(f"G1 X{x_move:.6f} F{move_rate}")
 
     for i in range(num_grooves):
         gcode.append(f"; Cut groove {i+1}")
-        gcode.append(f"G1 X{current_x + x_move:.6f} Z{-depth:.6f} F{feed_rate} ")
+        gcode.append(f"G1 Z{-depth:.6f} F{feed_rate} ")
         gcode.append(f"G1 X{current_x:.6f} Z{retraction_distance:.6f} F{feed_rate} ; Synchronized retraction")
         current_x += spacing
         gcode.append(f"G1 X{current_x + x_move:.6f} Z{retraction_distance:.6f} F{move_rate} ; Move to next groove starting position")
     
     return gcode
+
 
 
 
