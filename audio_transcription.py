d21d757ea0884a347712b55ac03f3084361000ed
bfufu <64049844+eponce92@users.noreply.github.com>
2023-09-02 14:15:10 -0700

    Update audio_transcription.py

--- audio_transcription.py
+++ audio_transcription.py
@@ -1,8 +1,10 @@
+
 import streamlit as st
 import openai
 from pytube import YouTube  # You can also use youtube_dl
 import re
 
+# Function to download YouTube video audio
 def download_audio(youtube_url):
     yt = YouTube(youtube_url)
     sanitized_title = re.sub(r'[^\w\s]', '', yt.title)
@@ -12,12 +14,14 @@ def download_audio(youtube_url):
     stream.download(filename=audio_file_path)
     return audio_file_path
 
+# Function to transcribe audio using Whisper API
 def whisper_transcribe(audio_file_path, api_key):
     openai.api_key = api_key
     audio_file = open(audio_file_path, "rb")
     transcript = openai.Audio.transcribe("whisper-1", audio_file)
     return transcript['text']
 
+# Function to continue the chat conversation
 def continue_chat(api_key, messages, model):
     openai.api_key = api_key
     response = openai.ChatCompletion.create(
@@ -27,24 +31,23 @@ def continue_chat(api_key, messages, model):
     return response['choices'][0]['message']['content']
 
 def main():
-    st.title("YouTube Video Transcriber and Assistant")
-    
+    st.title("YouTube Video Transcriber 📹")
+
     # Dropdown for GPT model selection
     gpt_model = st.selectbox(
         "Select GPT model:",
-        ("gpt-3.5-turbo", "gpt-3.5-turbo-16k")
+        ("gpt-3.5-turbo", "gpt-3.5-turbo-16k", "gpt-4", "gpt-4-32k")  # Add your accessible models here
     )
+
 
     # Input for OpenAI API Key
     openai_api_key = st.text_input("Enter your OpenAI API Key:", type="password")
     youtube_url = st.text_input("Enter YouTube Video URL:")
 
     if 'messages' not in st.session_state:
-        st.session_state['messages'] = [{"role": "system", "content": "You are a helpful assistant that understands the context of the audio transcription to chat with the user. The user might ask you to summarize or ask questions about the content of the transcription, and you should answer based on this information."}]
-    
-    chat_enabled = False  # Disable chat initially
+        st.session_state['messages'] = [{"role": "system", "content": "Use this audio transcription as context to chat with the user. The user might ask you to summarize or questions about the content of the transcription and you should answer based on this information."}]
 
-    # Button to start processing
+    # Button to start transcription
     if st.button("Transcribe"):
         if not openai_api_key or not youtube_url:
             st.warning("Please fill in all fields.")
@@ -55,26 +58,28 @@ def main():
                 
                 # Transcribe the video using Whisper API
                 transcription = whisper_transcribe(audio_file_path, openai_api_key)
-                st.session_state['messages'].append({"role": "assistant", "content": f"Transcription: {transcription}"})
 
                 with st.expander("Show Transcription"):
                     st.write(transcription)
+
+                # Add transcription to message history
+                st.session_state['messages'].append({"role": "assistant", "content": f"Transcription: {transcription}"})
                 
-                chat_enabled = True  # Enable chat after transcription
             except Exception as e:
                 st.error(f"An error occurred: {e}")
 
     # Chat Interface
-    st.write("## Continue Chatting with GPT")
+    if 'messages' in st.session_state and len(st.session_state['messages']) > 1:  # Check if transcription is done
+        st.write("## Continue Chatting with GPT")
 
-    # Display previous messages
-    for message in st.session_state['messages']:
-        role = message["role"]
-        content = message["content"]
-        with st.chat_message(role):
-            st.write(content)
+        # Display previous messages
+        for message in st.session_state['messages']:
+            role = message["role"]
+            content = message["content"]
+            with st.chat_message(role):
+                st.write(content)
 
-    if chat_enabled:  # Enable chat input only if chat is enabled
+        # User input
         user_input = st.chat_input("Type your message")
 
         if user_input:
