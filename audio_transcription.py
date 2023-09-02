d47a01b99c460ebd730f93df76d89c9ce5a62520
bfufu <64049844+eponce92@users.noreply.github.com>
2023-09-02 14:06:53 -0700

    Update audio_transcription.py

--- audio_transcription.py
+++ audio_transcription.py
@@ -12,27 +12,12 @@ def download_audio(youtube_url):
     stream.download(filename=audio_file_path)
     return audio_file_path
 
-# Function to transcribe audio using Whisper API
 def whisper_transcribe(audio_file_path, api_key):
     openai.api_key = api_key
     audio_file = open(audio_file_path, "rb")
     transcript = openai.Audio.transcribe("whisper-1", audio_file)
     return transcript['text']
 
-# Function to summarize text using ChatGPT API
-def gpt_summarize(text, api_key, model):
-    openai.api_key = api_key
-    messages = [
-        {"role": "system", "content": "You are a helpful assistant."},
-        {"role": "user", "content": f"Please summarize the following text: {text}"}
-    ]
-    response = openai.ChatCompletion.create(
-        model=model,
-        messages=messages
-    )
-    return response['choices'][0]['message']['content']
-
-# Function to continue the chat conversation
 def continue_chat(api_key, messages, model):
     openai.api_key = api_key
     response = openai.ChatCompletion.create(
@@ -42,24 +27,19 @@ def continue_chat(api_key, messages, model):
     return response['choices'][0]['message']['content']
 
 def main():
-    st.title("YouTube Video Summarizer")
-
-    # Dropdown for GPT model selection
-    gpt_model = st.selectbox(
-        "Select GPT model:",
-        ("gpt-3.5-turbo", "gpt-3.5-turbo-16k", "gpt-4", "gpt-4-32k")  # Add your accessible models here
-    )
-
+    st.title("YouTube Video Transcriber and Assistant")
 
     # Input for OpenAI API Key
     openai_api_key = st.text_input("Enter your OpenAI API Key:", type="password")
     youtube_url = st.text_input("Enter YouTube Video URL:")
 
     if 'messages' not in st.session_state:
-        st.session_state['messages'] = [{"role": "system", "content": "You are a helpful assistant."}]
+        st.session_state['messages'] = [{"role": "system", "content": "You are a helpful assistant that understands the context of the audio transcription to chat with the user. The user might ask you to summarize or ask questions about the content of the transcription and you should answer based on this information."}]
+    
+    chat_enabled = False  # Disable chat initially
 
     # Button to start processing
-    if st.button("Summarize"):
+    if st.button("Transcribe"):
         if not openai_api_key or not youtube_url:
             st.warning("Please fill in all fields.")
         else:
@@ -69,18 +49,12 @@ def main():
                 
                 # Transcribe the video using Whisper API
                 transcription = whisper_transcribe(audio_file_path, openai_api_key)
+                st.session_state['messages'].append({"role": "assistant", "content": f"Transcription: {transcription}"})
 
                 with st.expander("Show Transcription"):
                     st.write(transcription)
-
-                # Summarize the transcription using OpenAI GPT API
-                summary = gpt_summarize(transcription, openai_api_key, gpt_model)
-
-                with st.expander("Show Summary"):
-                    st.write(summary)
                 
-                # Add summary to message history
-                st.session_state['messages'].append({"role": "assistant", "content": f"Summary: {summary}"})
+                chat_enabled = True  # Enable chat after transcription
             except Exception as e:
                 st.error(f"An error occurred: {e}")
 
@@ -94,24 +68,24 @@ def main():
         with st.chat_message(role):
             st.write(content)
 
-    # User input
-    user_input = st.chat_input("Type your message")
+    if chat_enabled:  # Enable chat input only if chat is enabled
+        user_input = st.chat_input("Type your message")
 
-    if user_input:
-        try:
-            # Add user message to message history
-            st.session_state['messages'].append({"role": "user", "content": user_input})
+        if user_input:
+            try:
+                # Add user message to message history
+                st.session_state['messages'].append({"role": "user", "content": user_input})
 
-            # Get GPT response
-            gpt_response = continue_chat(openai_api_key, st.session_state['messages'], gpt_model)
+                # Get GPT response
+                gpt_response = continue_chat(openai_api_key, st.session_state['messages'], "gpt-3.5-turbo")  # Adjust the model as needed
 
-            # Add GPT message to message history
-            st.session_state['messages'].append({"role": "assistant", "content": gpt_response})
-            
-            with st.chat_message("assistant"):
-                st.write(gpt_response)
-        except Exception as e:
-            st.error(f"An error occurred: {e}")
+                # Add GPT message to message history
+                st.session_state['messages'].append({"role": "assistant", "content": gpt_response})
+                
+                with st.chat_message("assistant"):
+                    st.write(gpt_response)
+            except Exception as e:
+                st.error(f"An error occurred: {e}")
 
 if __name__ == "__main__":
     main()
