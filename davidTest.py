1df683ae6501fa6d8cd1c24b886aa157b22c12d5
bfufu <64049844+eponce92@users.noreply.github.com>
2023-09-16 20:25:46 -0700

    Update davidTest.py

--- davidTest.py
+++ davidTest.py
@@ -2,47 +2,12 @@ import streamlit as st
 from datetime import datetime, timedelta
 import calendar
 
-# Definición de skills y niveles
-skills = [
-    "Programacion de PLC",
-    "Cognex Cameras",
-    "Kistler press",
-    "Mechanical troubleshooting",
-    "Electrical schematics and troubleshooting",
-    "Keyence vision",
-    "Fanuc Programming",
-    "Atlas coptco nutrunners"
-]
-
-levels = {
-    0: "No tiene conocimiento",
-    1: "Tiene el conocimiento pero no tiene la práctica",
-    2: "Lo puede hacer pero con ayuda",
-    3: "Lo puede hacer solo sin ayuda",
-    4: "Puede entrenar a otros"
-}
-
-targets = {
-    "ingeniero asociado": 2,
-    "ingeniero": 3,
-    "ingeniero senior": 4
-}
-
-def generate_training_schedule(sorted_discrepancies):
-    start_date = datetime.now() + timedelta(weeks=1)
-    
-    # Aquí debemos filtrar las habilidades con discrepancia >= 1, no sólo las que son exactamente 1.
-    trainings = [skill for skill, discrepancy in sorted_discrepancies if discrepancy >= 1]
-    
-    training_dates = []
-    for i, training in enumerate(trainings):
-        training_date = start_date + timedelta(weeks=i*4)  # Sumamos 4 semanas para cada siguiente entrenamiento
-        training_dates.append((training, training_date))
-
-    return training_dates
-
-
+# Función para mostrar el calendario de entrenamiento
 def display_calendar(training_dates):
+    if not training_dates:
+        st.write("¡Buenas noticias! No se requieren entrenamientos adicionales.")
+        return
+    
     current_month = training_dates[0][1].month
     current_year = training_dates[0][1].year
 
@@ -53,40 +18,67 @@ def display_calendar(training_dates):
             st.text(cal.formatmonth(current_year, current_month))
         st.write(f"{date.strftime('%Y-%m-%d')} - Entrenamiento en: {training}")
 
+# Función para generar el calendario de entrenamiento
+def generate_training_schedule(sorted_discrepancies):
+    start_date = datetime.now() + timedelta(weeks=1)
+    trainings = [skill for skill, discrepancy in sorted_discrepancies if discrepancy >= 1]
+    
+    training_dates = []
+    for i, training in enumerate(trainings):
+        training_date = start_date + timedelta(weeks=i*4)
+        training_dates.append((training, training_date))
+
+    return training_dates
+
 def main():
-    st.title('Autoevaluación para Ingenieros de Tesla')
-    st.write("Por favor, rellena el formulario a continuación:")
+    st.title("Autoevaluación de habilidades para ingenieros en Tesla")
+    
+    skills = [
+        "Programacion de PLC",
+        "Cognex Cameras",
+        "Kistler press",
+        "Mechanical troubleshooting",
+        "Electrical schematics and troubleshooting",
+        "Keyence vision",
+        "Fanuc Programming",
+        "Atlas coptco nutrunners"
+    ]
 
-    # Capturar datos del formulario
-    name = st.text_input("Nombre:")
-    position = st.selectbox("Selecciona tu posición:", list(targets.keys()))
-    user_ratings = {}
+    levels = [
+        "No tiene conocimiento",
+        "Tiene el conocimiento pero no tiene la práctica",
+        "Lo puede hacer pero con ayuda",
+        "Lo puede hacer solo sin ayuda",
+        "Puede entrenar a otros"
+    ]
+
+    st.subheader("Completa el formulario de autoevaluación")
+    engineer_name = st.text_input("Nombre del ingeniero", "")
+    engineer_level = st.selectbox("Nivel del ingeniero", ["Ingeniero Asociado", "Ingeniero", "Ingeniero Senior"])
+    
+    evaluations = {}
     for skill in skills:
-        rating = st.selectbox(f"Califica tu habilidad en {skill}", list(levels.keys()), format_func=lambda x: levels[x])
-        user_ratings[skill] = rating
+        evaluations[skill] = st.selectbox(f"Evalúa tu habilidad en: {skill}", options=levels, index=0)
 
-    if st.button("Enviar"):
-        if name:
-            # Procesar los resultados
-            discrepancies = {}
-            for skill, rating in user_ratings.items():
-                discrepancy = rating - targets[position]
-                discrepancies[skill] = discrepancy
+    if st.button("Enviar autoevaluación"):
+        discrepancies = {}
+        
+        # Establecer el nivel objetivo basado en el nivel del ingeniero
+        target_level = 2 if engineer_level == "Ingeniero Asociado" else 3 if engineer_level == "Ingeniero" else 4
 
-            # Ordenar por criticidad
-            sorted_discrepancies = sorted(discrepancies.items(), key=lambda x: x[1], reverse=True)
+        for skill, level in evaluations.items():
+            skill_discrepancy = target_level - levels.index(level)
+            discrepancies[skill] = skill_discrepancy
 
-            # Mostrar resultados
-            st.subheader(f"Resultados de autoevaluación de {name} ({position}):")
-            for skill, discrepancy in sorted_discrepancies:
-                st.write(f"{skill}: {levels[user_ratings[skill]]} (Diferencia: {discrepancy})")
-
-            # Generar el calendario de entrenamiento
-            training_dates = generate_training_schedule(sorted_discrepancies)
-            display_calendar(training_dates)
-            
-        else:
-            st.warning("Por favor, ingresa tu nombre.")
+        sorted_discrepancies = sorted(discrepancies.items(), key=lambda x: x[1], reverse=True)
+        
+        st.subheader(f"Resultados de autoevaluación de {engineer_name} ({engineer_level}):")
+        for skill, discrepancy in sorted_discrepancies:
+            st.write(f"{skill}: {evaluations[skill]} (Diferencia: {discrepancy})")
+        
+        training_dates = generate_training_schedule(sorted_discrepancies)
+        st.subheader("Calendario de entrenamiento:")
+        display_calendar(training_dates)
 
 if __name__ == "__main__":
     main()
