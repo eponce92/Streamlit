d8854a85e4d30f93cb3ce653797d3cb179b7ece2
bfufu <64049844+eponce92@users.noreply.github.com>
2023-09-18 16:01:08 -0700

    Update davidTest.py

--- davidTest.py
+++ davidTest.py
@@ -7,54 +7,30 @@ from email.mime.base import MIMEBase
 from email import encoders
 
 LEVELS = ['No knowledge', 'Knows but no practice', 'Can do with help', 'Can do alone', 'Can teach others']
+TARGETS = {dict(zip(correct_target_levels, range(len(correct_target_levels))))}
+SKILLS = {skills}
+TARGET_VALUES = {target_values}
+
 SHOW_RESULTS = True  # Change this to False if you don't want to show the results
 last_submitted_name = None  # Keep track of the last name submitted
 
-@st.cache(allow_output_mutation=True)
-def get_data_from_excel(file):
-    df = pd.read_excel(file)
-    
-    # Extract skill list
-    skills = df['C'].dropna().tolist()
-    
-    # Extract target levels
-    targets = {col: idx for idx, col in enumerate(df.columns[3:].tolist())}
-    
-    return skills, targets
+@st.cache_data 
+def get_skills():
+    return SKILLS
+
+def get_target_value(skill, position):
+    skill_index = SKILLS.index(skill)
+    position_index = list(TARGETS.keys()).index(position)
+    return TARGET_VALUES[skill_index][position_index]
 
 def send_email(name, position, results_data):
-    ...
-    # (The email sending function remains unchanged.)
-
-def main():
-    st.title("Engineer Auto-Evaluation")
+    global last_submitted_name
+    if name == last_submitted_name:
+        st.warning("Duplicate submission detected. Email not sent.")
+        return
+    last_submitted_name = name
     
-    uploaded_file = st.file_uploader("Upload Training Matrix Excel File", type="xlsx")
-    
-    if uploaded_file:
-        skills, TARGETS = get_data_from_excel(uploaded_file)
-        
-        name = st.text_input("Your Name:")
-        position = st.selectbox("Your Engineer Level:", list(TARGETS.keys()))
-
-        responses = {}
-    
-        for skill in skills:
-            responses[skill] = st.selectbox(f"How would you rate your {skill} skills?", LEVELS)
-
-        if st.button("Submit"):
-            results_data = []
-            for skill, level in responses.items():
-                difference = LEVELS.index(level) - TARGETS[position]
-                results_data.append([skill, level, difference])
-
-            send_email(name, position, results_data)
-
-            if SHOW_RESULTS:
-                results_df = pd.DataFrame(results_data, columns=['Skill', 'Self-Assessment', 'Difference'])
-                st.write(results_df.to_html(index=False, classes='table table-striped table-hover'), unsafe_allow_html=True)
-    else:
-        st.warning("Please upload the Training Matrix Excel file to proceed.")
+    # ... rest of the code remains unchanged ...
 
 if __name__ == "__main__":
     main()
