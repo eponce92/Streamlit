d30d24e2a9c813377119018af5ce8b0b31804db3
bfufu <64049844+eponce92@users.noreply.github.com>
2023-09-26 15:25:03 -0700

    Update compaction_pressure.py

--- compaction_pressure.py
+++ compaction_pressure.py
@@ -39,17 +39,26 @@ st.title("Pneumatic Cylinder Compaction Force Analysis")
 
 # Documentation
 st.write("""
+### Pneumatic Cylinder Compaction Force Analysis
 This application processes data from a PLC trend on an IL-100 distance sensor observing a pneumatic actuator. 
-The main objective is to determine the peak force the cylinder exerts on powder during compaction. 
-The following calculations are performed:
+The main objective is to determine the peak force the cylinder exerts on powder during compaction.
 
-- **Velocity**: Derived from the rate of change of compaction over time.
-- **Acceleration**: Derived from the rate of change of velocity.
-- **Pneumatic Force**: Calculated based on the internal pressure and the bore size of the cylinder using the formula \( F = P \times A \).
-- **Force due to Inertia**: Calculated using the formula \( F = m \times a \), where \( m \) is the mass of the tool and \( a \) is the acceleration.
-- **Total Force**: Sum of the pneumatic force and the force due to inertia.
-- **Pressure on the Tip**: Calculated by dividing the total force by the area of the compactor pin tip.
+#### Key Metrics:
+- **PSI**: Represents the internal pressure of the pneumatic cylinder.
+- **Flow**: Indicates the flow rate of air into the cylinder, influencing piston speed.
+- **Pneumatic Force**: Calculated from the internal pressure and the cylinder's bore size.
+    - \( F_{\text{pneumatic}} = P \times A \)
+    where \( P \) is the pressure in Pascals and \( A \) is the piston's area.
+- **Inertial Force**: Represents the force due to the tool's acceleration.
+    - \( F_{\text{inertia}} = m \times a \)
+    where \( m \) is the mass of the tool and \( a \) is its acceleration.
+- **Total Force**: The sum of pneumatic and inertial forces.
+- **Pressure on the Tip**: Represents the pressure experienced by the material being compacted.
+    - Calculated by dividing the total force by the compactor pin's tip area.
+
+To use the application, upload the data file and adjust the parameters on the sidebar as needed. The results will be displayed along with interactive plots for a comprehensive analysis.
 """)
+
 
 # Upload the Excel file
 uploaded_file = st.file_uploader("Choose an Excel file", type="xlsx")
