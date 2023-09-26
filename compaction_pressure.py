154156bfee4e64ae06a02145226bff91f88dcb49
bfufu <64049844+eponce92@users.noreply.github.com>
2023-09-26 12:59:49 -0700

    Update compaction_pressure.py

--- compaction_pressure.py
+++ compaction_pressure.py
@@ -1,4 +1,3 @@
-
 import streamlit as st
 import pandas as pd
 import numpy as np
@@ -6,6 +5,7 @@ import math
 import matplotlib.pyplot as plt
 
 def process_data(data, pressure, bore_size, mass, tip_diameter):
+    """Process the uploaded data and compute required metrics."""
     # Convert Time and Milisecond columns to a single time in milliseconds
     data['Total Time (ms)'] = data['Milisecond']
 
@@ -22,16 +22,39 @@ def process_data(data, pressure, bore_size, mass, tip_diameter):
     F_inertia = mass * data['Smoothed Acceleration (mm/ms^2)'].abs().max() * 1000  # In m/s^2
     F_total = F_pneumatic + F_inertia
 
-    return F_pneumatic, F_inertia, F_total
+    # Calculate pressure on the tip
+    tip_area_mm2 = math.pi * (tip_diameter / 2)**2  # Area in mm^2
+    F_total_lbs = F_total * 0.2248  # Force in lbs
+    pressure_tip_psi = F_total_lbs / (tip_area_mm2 / 645.16)  # Pressure in PSI
+
+    return F_pneumatic, F_inertia, F_total, pressure_tip_psi
 
 st.title("Pneumatic Cylinder Compaction Force Analysis")
+
+# Documentation
+st.write("""
+This application processes data from a PLC trend on an IL-100 distance sensor observing a pneumatic actuator. 
+The main objective is to determine the peak force the cylinder exerts on powder during compaction. 
+The following calculations are performed:
+
+- **Velocity**: Derived from the rate of change of compaction over time.
+- **Acceleration**: Derived from the rate of change of velocity.
+- **Pneumatic Force**: Calculated based on the internal pressure and the bore size of the cylinder using the formula \( F = P \times A \).
+- **Force due to Inertia**: Calculated using the formula \( F = m \times a \), where \( m \) is the mass of the tool and \( a \) is the acceleration.
+- **Total Force**: Sum of the pneumatic force and the force due to inertia.
+- **Pressure on the Tip**: Calculated by dividing the total force by the area of the compactor pin tip.
+""")
 
 # Upload the Excel file
 uploaded_file = st.file_uploader("Choose an Excel file", type="xlsx")
 
 if uploaded_file:
-    data = pd.read_excel(uploaded_file)
-    st.write("Data uploaded successfully!")
+    try:
+        data = pd.read_excel(uploaded_file)
+        st.write("Data uploaded successfully!")
+    except Exception as e:
+        st.write("Error reading the uploaded file. Please ensure the file format and content are correct.")
+        st.stop()
 
     # Parameters input
     st.sidebar.header("Parameters")
@@ -41,11 +64,14 @@ if uploaded_file:
     tip_diameter = st.sidebar.number_input("Compactor Pin Diameter (thou)", min_value=0.0, value=25.0, step=0.1) * 0.0254  # in meters
 
     # Process data and calculate forces
-    F_pneumatic, F_inertia, F_total = process_data(data, pressure, bore_size, mass, tip_diameter)
+    F_pneumatic, F_inertia, F_total, pressure_tip_psi = process_data(data, pressure, bore_size, mass, tip_diameter)
 
-    st.write(f"Pneumatic Force: {F_pneumatic:.2f} N")
-    st.write(f"Force due to Inertia: {F_inertia:.2f} N")
-    st.write(f"Total Force: {F_total:.2f} N")
+    # Display results
+    st.header("Results:")
+    st.write(f"**Pneumatic Force**: {F_pneumatic:.2f} N")
+    st.write(f"**Force due to Inertia**: {F_inertia:.2f} N")
+    st.write(f"**Total Force**: {F_total:.2f} N")
+    st.write(f"**Pressure on the Compaction Pin Tip**: {pressure_tip_psi:.2f} PSI")
 
     # Plotting
     fig, ax = plt.subplots(figsize=(10, 6))
