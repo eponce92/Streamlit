add73d616f747a6512e31fe0f7f17ecdb3f2afe3
bfufu <64049844+eponce92@users.noreply.github.com>
2023-10-10 09:39:29 -0700

    Update money_send_test.py

--- money_send_test.py
+++ money_send_test.py
@@ -2,51 +2,48 @@ import streamlit as st
 from streamlit_gsheets import GSheetsConnection
 import pandas as pd
 
-st.title("Visualización y Adición a Google Sheets")
+st.title("Article Price Database")
 
-# Crea la conexión a GSheets
+# Establish the connection to Google Sheets
 conn = st.experimental_connection("gsheets", type=GSheetsConnection)
 
-# Lee la hoja de cálculo de Google como un DataFrame
+# Read the current data from the Google Sheet
 df = conn.read(
     worksheet="Sheet1",
-    usecols=[0, 1]  # Usar solo las dos primeras columnas
+    usecols=[0, 1]  # Assuming data is stored in the first two columns
 )
 
-# Muestra el DataFrame actual en Streamlit
-st.subheader("Contenido actual de la hoja:")
+# Display current data to the user
+st.subheader("Current Database")
 st.dataframe(df)
 
-# Formulario para agregar una nueva entrada
-with st.form(key='new_entry_form'):
-    st.subheader("Añadir una nueva entrada")
+# User input for new entries
+st.subheader("Add a New Entry")
+article_name = st.text_input("Article Name")
+price = st.number_input("Price", min_value=0.0, step=0.01)
+
+# Button to add the new entry to the Google Sheet
+if st.button("Add Entry"):
+    new_entry = pd.DataFrame({
+        'Article Name': [article_name],
+        'Price': [price]
+    })
+
+    # Re-read the Google Sheet to ensure we're working with the most recent version
+    df_current = conn.read(
+        worksheet="Sheet1",
+        usecols=[0, 1]
+    )
+    df_current.dropna(how='all', inplace=True)
+
+    # Add the new entry and update the Google Sheet
+    df_updated = pd.concat([df_current, new_entry], ignore_index=True)
     
-    new_name = st.text_input("Nombre")
-    new_age = st.number_input("Edad", min_value=0)
+    # Clear the worksheet before updating with new data
+    conn.clear(worksheet="Sheet1")
+    
+    # Push the updated DataFrame to the Google Sheet
+    conn.create(worksheet="Sheet1", data=df_updated)
 
-    # Cuando se envíe el formulario, agregue la nueva entrada al DataFrame y luego a Google Sheets
-    submit_button = st.form_submit_button("Añadir")
-    if submit_button:
-        new_entry = pd.DataFrame({
-            'Names': [new_name],
-            'Ages': [new_age]
-        })
-
-        # Elimina las filas vacías y añade la nueva entrada al DataFrame
-        df.dropna(how='all', inplace=True)
-        df = pd.concat([df, new_entry], ignore_index=True)
-
-        # Limpia la hoja completamente
-        conn.clear(worksheet="Sheet1")
-
-        # Actualiza la hoja con el DataFrame actualizado
-        conn.update(
-            worksheet="Sheet1",
-            data=df
-        )
-
-        # Limpia el caché de datos y recarga la app
-        st.cache_data.clear()
-        st.experimental_rerun()
-
-        st.success("Entrada añadida con éxito!")
+    st.success("Entry Added Successfully!")
+    st.experimental_rerun()  # Reload the app to display the updated data
