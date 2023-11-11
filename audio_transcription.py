c9c543feb1ff0b6f7f3f97228abd256ebeb95584
bfufu <64049844+eponce92@users.noreply.github.com>
2023-11-10 17:51:13 -0800

    Update audio_transcription.py

--- audio_transcription.py
+++ audio_transcription.py
@@ -23,16 +23,16 @@ def download_audio(youtube_url):
     return audio_file_path
 
 # Function to transcribe audio using Whisper API
-
 def whisper_transcribe(audio_file_path, api_key):
-    openai.api_key = api_key
+    client = OpenAI(api_key=api_key)
     with open(audio_file_path, "rb") as audio_file:
-        transcript = openai.Audio.transcriptions.create(
+        transcript = client.audio.transcriptions.create(
             model="whisper-1", 
             file=audio_file,
             response_format="text"
         )
-    return transcript['text']
+    return transcript['data']['text']
+
     
 # Function to continue the chat conversation
 def continue_chat(api_key, messages, model):
